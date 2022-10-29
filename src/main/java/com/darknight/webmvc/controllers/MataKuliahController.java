53f9291f923be73525f90ce8d588b1e79a510c8a
todikun <todirahmat123@gmail.com>
2022-10-29 20:38:25 +0700

    feat: Dosen controller, view, service

--- src/main/java/com/darknight/webmvc/controllers/MataKuliahController.java
+++ src/main/java/com/darknight/webmvc/controllers/MataKuliahController.java
@@ -4,10 +4,7 @@ import com.darknight.webmvc.models.MataKuliahModel;
 import com.darknight.webmvc.services.MataKuliahService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -39,6 +36,47 @@ public class MataKuliahController {
     @PostMapping("/save")
     public ModelAndView save(@ModelAttribute MataKuliahModel request) {
         this.service.save(request);
+        return new ModelAndView("redirect:/matakuliah");
+    }
+
+    @GetMapping("/detail/{id}")
+    public ModelAndView detail(@PathVariable("id") String id) {
+        MataKuliahModel mataKuliah = service.getById(id);
+        if (mataKuliah == null) {
+            return new ModelAndView("redirect:/matakuliah");
+        }
+
+        ModelAndView view = new ModelAndView("matakuliah/detail.html");
+        view.addObject("data", mataKuliah);
+        return view;
+    }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView edit(@PathVariable("id") String id) {
+        MataKuliahModel mataKuliah = service.getById(id);
+        if (mataKuliah == null) {
+            return new ModelAndView("redirect:/matakuliah");
+        }
+
+        ModelAndView view = new ModelAndView("matakuliah/edit.html");
+        view.addObject("data", mataKuliah);
+        return view;
+    }
+
+    @PostMapping("/update")
+    public ModelAndView update(@ModelAttribute MataKuliahModel request) {
+        this.service.update(request.getId(), request);
+        return new ModelAndView("redirect:/matakuliah");
+    }
+
+    @PostMapping("/delete")
+    public ModelAndView delete(@ModelAttribute MataKuliahModel request) {
+        MataKuliahModel mataKuliah = service.getById(request.getId());
+        if (mataKuliah == null) {
+            return new ModelAndView("redirect:/matakuliah");
+        }
+
+        this.service.delete(request.getId());
         return new ModelAndView("redirect:/matakuliah");
     }
 
